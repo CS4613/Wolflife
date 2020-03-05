4ff45ea542737a5d99fb3d7aeeaec310e73c9437
cadepowers99 <cadepowers99@gmail.com>
2020-03-05 16:08:51 -0600

    Lessons Format

--- WolfLife/WolfLife.Android/obj/Debug/90/android/environment.x86_64.s
+++ WolfLife/WolfLife.Android/obj/Debug/90/android/environment.x86_64.s
@@ -62,7 +62,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"866796f9-836f-429d-be8b-f46f4e1dd192"
+	.asciz	"b8b7b8dd-16f8-4bf9-af14-8d2511cd927c"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
